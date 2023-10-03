d3f8eaff2ab9e9b673a8496852044b43ed12cbd0
David Burnett <burnett.david93@gmail.com>
2023-10-03 14:33:03 -0400

    update

--- backend/server.js
+++ backend/server.js
@@ -31,13 +31,11 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, 'frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(
-//     path.join(__dirname, 'ECommerce-WebApp/frontend/build/index.html')
-//   )
-// );
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'frontend/build/index.html'))
+);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
